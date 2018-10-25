c2638f989a757ea1bd8f1b80ba61c71599cded6a
Scoobert2bert <willcass@gmail.com>
2018-10-25 14:49:43 -0400

    now functional. Needs clean up.

--- cycles.py
+++ cycles.py
@@ -5,7 +5,6 @@ import ast #ast allows for easy parsing of input string into list/int
 a = ast.literal_eval(sys.argv[1])
 b = a[:]
 moving = []
-
 cycle = []
 long = []
 temp = []
@@ -25,25 +24,16 @@ def clean_cycles():
         if len(cycle[clean1]) > 1:
             long.append(cycle[clean1])
 
-def out(count):
-    wow = 0
-    while wow < len(long):
-        if all(elem in long[done]  for elem in long[wow]):
-              count = count + 1
-
-        if count > 1:
-            del long[wow]
-            return 0
-        wow = wow+1
-
 establish_identity()
 orig,current = moving[:],moving[:]
 
 for findloops in range(1,len(a)+1):
     temp = []
+    moving,current = orig[:],orig[:]
     temp.append(findloops)
-    for i in range(0,len(a)):
-        power()
+    power()
+    i = 0
+    while moving[:] != orig[:]:
         current = moving[:]
         for m in [k for k,j in enumerate(moving) if j == findloops]:
             pass
@@ -56,25 +46,63 @@ for findloops in range(1,len(a)+1):
         else:
             temp.append(m+1)
 
+        power()
+        i = i + 1
 
     cycle.append(temp)
 
+print(cycle)
 clean_cycles()
 
+k = []
 for finish in range(0,len(long)):
-    long[finish].sort(key=int)
+    k.append(sum(i*i for i in long[finish]))
 
-done = 0
-while done < len(long):
+print(k)
+count=0
+kill1 = []
+for soclose in range(0,len(k)):
     count = 0
-    out(count)
-    done = done+1
+    for iii in range(0,len(k)):
+        if k[soclose] == k[iii]:
+            count = count+1
 
-done = 0
-while done < len(long):
-    count = 0
-    out(count)
-    done = done+1
+        print(count)
+        if count > 1:
+            kill1.append(iii)
 
-out1 = str(long)
-sys.stdout.write(out1)
+kill1 = set(kill1)
+kill1 = list(kill1)
+print(kill1)
+
+for end2 in range(len(kill1)-1,-1,-1):
+    del long[kill1[end2]]
+
+print(k)
+print(long)
+
+print(k)
+count=0
+kill1 = []
+
+if len(long) != 1 and len(long) != 0:
+    for soclose in range(0,len(long)):
+        count = 0
+        for iii in range(0,len(long)):
+            if all(elem in long[soclose] for elem in long[iii]):
+                count = count+1
+
+            print(count)
+            if count > 1:
+                kill1.append(iii)
+
+    kill1 = set(kill1)
+    kill1 = list(kill1)
+    print(kill1)
+
+for end2 in range(len(kill1)-1,-1,-1):
+    del long[kill1[end2]]
+
+for i in range(0,len(long)):
+    out1 = str(long[i])
+    sys.stdout.write(out1)
