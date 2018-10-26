773d9c1b5c01bfc3fb5138081cc154267c538b65
Scoobert2bert <willcass@gmail.com>
2018-10-25 21:40:41 -0400

    Now entirely functional. Cleaned up and commented.

--- hw3/cycles.py
+++ hw3/cycles.py
@@ -24,10 +24,10 @@ def clean_cycles():
         if len(cycle[clean1]) > 1:
             long.append(cycle[clean1])
 
-establish_identity()
+establish_identity()#Dynamically establish identity permutations
 orig,current = moving[:],moving[:]
 
-for findloops in range(1,len(a)+1):
+for findloops in range(1,len(a)+1): #starts at each number and follows the path it takes as it moves.
     temp = []
     moving,current = orig[:],orig[:]
     temp.append(findloops)
@@ -49,60 +49,25 @@ for findloops in range(1,len(a)+1):
         power()
         i = i + 1
 
-    cycle.append(temp)
-
-print(cycle)
+    cycle.append(temp)#list of lists containing how each number moves
+                      #There will be duplicates. Now must remove duplicates
 clean_cycles()
 
-k = []
-for finish in range(0,len(long)):
-    k.append(sum(i*i for i in long[finish]))
-
-print(k)
 count=0
-kill1 = []
-for soclose in range(0,len(k)):
-    count = 0
-    for iii in range(0,len(k)):
-        if k[soclose] == k[iii]:
-            count = count+1
-
-        print(count)
-        if count > 1:
-            kill1.append(iii)
-
-kill1 = set(kill1)
-kill1 = list(kill1)
-print(kill1)
-
-for end2 in range(len(kill1)-1,-1,-1):
-    del long[kill1[end2]]
-
-print(k)
-print(long)
-
-print(k)
-count=0
-kill1 = []
-
 if len(long) != 1 and len(long) != 0:
-    for soclose in range(0,len(long)):
+    for soclose in range(len(long)-1,-1,-1):
         count = 0
-        for iii in range(0,len(long)):
-            if all(elem in long[soclose] for elem in long[iii]):
+        for iii in range(len(long)-1,-1,-1):
+            if set(long[soclose]).issubset(set(long[iii])):
                 count = count+1
 
-            print(count)
             if count > 1:
-                kill1.append(iii)
-
-    kill1 = set(kill1)
-    kill1 = list(kill1)
-    print(kill1)
-
-for end2 in range(len(kill1)-1,-1,-1):
-    del long[kill1[end2]]
-
+                long[soclose] = [0] #If dup found replace it with a one element list
+                                    #Simple to filter out at the end
+for clean1 in range(len(long)-1,-1,-1):
+        if len(long[clean1]) == 1:
+            del long[clean1]
+            
 for i in range(0,len(long)):
     out1 = str(long[i])
     sys.stdout.write(out1)
